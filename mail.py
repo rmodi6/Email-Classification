b309433c50e0cb8201be407e025b68116c0cceb9
Ruchit Modi <modi.ruchit6@gmail.com>
2016-05-08 21:11:09 +0530

    to fetch live emails from an email account

---
+++ mail.py
@@ -0,0 +1,109 @@
+import os
+import sys
+import imaplib
+import email
+
+class Mail():
+
+	GMAIL_IMAP_HOST = 'imap.gmail.com'
+	OUTLOOK_IMAP_HOST = 'imap-mail.outlook.com'
+
+	def __init__(self):
+		self.username = None
+		self.password = None
+		self.imap = None
+	
+	def login(self, username, password):
+		self.username = username
+		self.password = password
+		self.imap = imaplib.IMAP4_SSL(self.OUTLOOK_IMAP_HOST)
+
+		print('Trying to login...')
+		try:
+			response, data = self.imap.login(self.username, self.password)
+			if response == 'OK':
+				print('Login Successful.')
+				return True
+		except imaplib.IMAP4.error:
+		    print("Authentication Error!")
+		    return False
+
+	def logout(self):
+		return self.imap.logout()
+
+	def list(self):
+		response, results = self.imap.list()
+		if response == 'OK':
+			return results
+
+	def select(self, mailbox_name):
+		response, data = self.imap.select(mailbox_name)
+		if response == 'OK':
+			return True
+		else:
+			print(mailbox_name + ' folder not found.')
+			return False
+
+	def fetch_mailbox(self, path, mailbox_name):
+		folder_path = path + mailbox_name + "\\"
+		if not os.path.exists(folder_path):
+			os.mkdir(folder_path)
+
+		response, data = self.imap.select(mailbox_name)
+		response, data = self.imap.search(None, "ALL")
+		if response != 'OK':
+			print('No messages found in the folder ' + mailbox_name)
+			return
+
+		count, total = 1, len(data[0].split())
+
+		for id in data[0].split():
+			response, data = self.imap.fetch(id, '(RFC822)')
+			if response != 'OK':
+				print ("ERROR getting message", id)
+				return
+
+			msg = email.message_from_string(data[0][1].decode("utf-8"))
+			
+			outfile = open(folder_path + str(count) + ".txt", 'w') # open file for appending
+			outfile.write("Subject: " + msg['Subject'] + "\n")
+			# print ('Message %s: %s' % (id, msg['Subject']))
+
+			for part in msg.walk():
+				if part.get_content_type() == 'text/plain':
+					outfile.write("Content-Type: text/plain; charset=utf-8"+"\nx-filename: "+str(count)+"\n\n"+part.get_payload())
+					# print (part.get_payload()) 
+					# body.append(part.get_payload())
+			count += 1
+
+	def fetch_unread(self, path):
+		response, data = self.imap.select("Inbox")
+		response, data = self.imap.search(None, "UNSEEN")
+		if response != 'OK':
+			print('No new messages found.')
+			return
+
+		count, total = 1, len(data[0].split())
+
+		for id in data[0].split():
+			response, data = self.imap.fetch(id, '(RFC822)')
+			if response != 'OK':
+				print ("ERROR getting message", id)
+				return
+
+			msg = email.message_from_string(data[0][1].decode("utf-8"))
+			
+			outfile = open(path + str(count) + ".txt", 'w') # open file for appending
+			outfile.write("Subject: " + msg['Subject'] + "\n")
+			# print ('Message %s: %s' % (id, msg['Subject']))
+
+			for part in msg.walk():
+				if part.get_content_type() == 'text/plain':
+					outfile.write("Content-Type: text/plain; charset=utf-8"+"\nx-filename: "+str(count)+"\n\n"+part.get_payload())
+					# print (part.get_payload()) 
+					# body.append(part.get_payload())
+			count += 1
+
+if __name__ == '__main__':
+	e = Mail()
+	e.login(usr, pwd)
